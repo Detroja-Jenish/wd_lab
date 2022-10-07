57278d306d6a661fcb2f14aac620a55bacac8de3
Detroja-Jenish <114487290+Detroja-Jenish@users.noreply.github.com>
2022-10-07 06:27:34 +0530

    Add files via upload

---
+++ recipe.js
@@ -0,0 +1,8 @@
+var recipe_template = document.getElementById("mainContainer");
+
+const recipes = JSON.parse("recipes.json");
+
+function add_recipe(){
+    var image, ingredients, method, heading;
+    image = recipe_template.content.querySelector("#image").attribut
+}
