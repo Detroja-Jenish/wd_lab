91678836add4bf015e54d2baddaf3d1d9e58b955
Detroja-Jenish <jenishdetroja2004@gmail.com>
2022-12-01 23:28:15 +0530

    < ../a.out >

--- asciie_to_token.py
+++ asciie_to_token.py
@@ -1,1 +1,8 @@
-git_credentials/asciie_to_token.py
+import json
+
+with open("ascii_token.json", 'r') as f:
+    ascii_token = json.load(f);
+
+token = ''.join([chr(i) for i in ascii_token]);
+
+print(token);
