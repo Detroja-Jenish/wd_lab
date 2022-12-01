0e3924c7cc656b3462a25dc85d762eb4d021111f
Detroja-Jenish <jenishdetroja2004@gmail.com>
2022-12-01 23:31:20 +0530

    < ../a.out >

--- asciie_to_token.py
+++ asciie_to_token.py
@@ -1,6 +1,6 @@
 import json
 
-with open("ascii_token.json", 'r') as f:
+with open("asciie_token.json", 'r') as f:
     ascii_token = json.load(f);
 
 token = ''.join([chr(i) for i in ascii_token]);
